7a301d284441b61ec257ebd05685b9869373bb54
james <jcjmcclean@gmail.com>
2020-01-17 15:53:59 +0000

    feat: global sass functionality added

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+module.exports = function override(config, env) {
+	if (!config.module.rules) {
+		config.module.rules = [];
+	}
+
+	config.module.rules.push({
+		test: /\.scss$/,
+		use: [
+			'sass-loader',
+			{
+				loader: 'sass-resources-loader',
+				options: {
+					resources: ['./assets/sass/app.scss']
+				}
+			}
+		]
+	});
+
+	return config;
+};
